04f72838cb19f4890604ddf4267e6d9ca1ee6c41
Sambhav Satija <sambhav13085@iiitd.ac.in>
2015-09-11 12:25:01 +0530

    init commit of merging attributes

---
+++ project/app/management/commands/create_main_attributes_file.py
@@ -0,0 +1,20 @@
+from glob import glob
+from progressbar import ProgressBar
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Create a main attribute file after merging all small attribute files'
+
+    def handle(self, *args, **options):
+        final_dict = {}
+        files = glob("attr_*")
+        pbar = ProgressBar(maxval=len(files) - 1).start()
+        for i, filename in enumerate(files):
+            with open(filename, 'r') as f:
+                data = f.read()
+                subset = eval(data) # hope no one is evil
+                final_dict.update(subset)
+                pbar.update(i)
+        pbar.finish()
+        print final_dict, len(final_dict)
